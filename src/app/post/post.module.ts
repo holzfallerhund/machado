e006a73b90e604613f1f7479a7cd697fca2f3991
luizedumachado <luizedumachadodemachado@gmail.com>
2019-06-23 20:59:08 -0300

    Comentários show

--- src/app/post/post.module.ts
+++ src/app/post/post.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { PostPage } from './post.page';
+import { CommentService } from './comment.service';
 
 const routes: Routes = [
   {
@@ -21,6 +22,9 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [PostPage]
+  declarations: [PostPage],
+  providers : [
+    CommentService
+  ]
 })
 export class PostPageModule {}
--- src/app/post/post.page.ts
+++ src/app/post/post.page.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { LoadingController, NavController } from '@ionic/angular';
+import { IComment, CommentService } from './comment.service';
 
 export interface Item { title: string }
 
@@ -16,17 +17,33 @@ export interface Item { title: string }
 export class PostPage {
 
   private itemsCollection: AngularFirestoreDocument<Item>;
-  private item: Observable<Item>
+  private item: Observable<Item>;
   private postID: string;
+  comments;
+  newcomment: IComment =  {  commmentContent: "", postId: "" }
 
   constructor(
     private route : ActivatedRoute, 
     private afs: AngularFirestore,
-    public navCtrl: NavController
+    public navCtrl: NavController,
+    private provider: CommentService
   ) {     
     this.postID = this.route.snapshot.paramMap.get('id')
     this.itemsCollection = afs.doc<Item>('posts/'+this.postID)
     this.item = this.itemsCollection.valueChanges()
+
+    this.provider.read_Comments(this.postID).subscribe(data => {
+  
+      this.comments = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          commentContent: e.payload.doc.data()['comment'],
+          commentPostId: e.payload.doc.data()['postId'],
+        };
+      })
+      console.log(this.comments);
+  
+    });
 
   }
 
@@ -34,5 +51,18 @@ export class PostPage {
     this.navCtrl.navigateBack('tabs/tab2');
   }
   
+  comment(){
+    let record = {};
+    record['comment'] = this.newcomment.commmentContent;
+    record['postId'] = this.postID;
+    this.provider.create_NewComment(record).then(resp => {
+      this.newcomment.commmentContent = "";
+      console.log(resp);
+    })
+      .catch(error => {
+        console.log(error);
+      });
 
+      
+  }
 }
