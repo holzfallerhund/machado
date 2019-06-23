ce47cd1aaa101ca54fd249093acb3ff2dc1b0f59
luizedumachado <luizedumachadodemachado@gmail.com>
2019-06-23 10:52:28 -0300

    lista

--- src/app/post/post.ts
+++ src/app/post/post.ts
@@ -1,9 +1,45 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import 'rxjs/add/operator/map';
+
+@Injectable()
 export class Post {
-
-    constructor(
-      public title: string,
-      public subject: string,
-      public description: string
-    ) {  }
-
+  private PATH = 'posts/';
+ 
+  constructor(private db: AngularFireDatabase) {
   }
+ 
+  getAll() {
+    return this.db.list(this.PATH, ref => ref.orderByChild('title'))
+      .snapshotChanges()
+      .map(changes => {
+        return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+      })
+  }
+ 
+  get(key: string) {
+    return this.db.object(this.PATH + key).snapshotChanges()
+      .map(c => {
+        return { key: c.key, ...c.payload.val() };
+      });
+  }
+ 
+  save(post: any) {
+    return new Promise((resolve, reject) => {
+      if (post.key) {
+        this.db.list(this.PATH)
+          .update(post.key, { title: post.title, description: post.description, subject: post.subject })
+          .then(() => resolve())
+          .catch((e) => reject(e));
+      } else {
+        this.db.list(this.PATH)
+          .push({ title: post.title, description: post.description, subject: post.subject })
+          .then(() => resolve());
+      }
+    })
+  }
+ 
+  remove(key: string) {
+    return this.db.list(this.PATH).remove(key);
+  }
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController} from '@ionic/angular';
+import { Post } from '../post/post';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
+  ngOnInit(): void {
+    this.posts = this.provider.getAll();
+  }
+
+  posts: Observable<any>;
 
   constructor(
-    public navCtrl: NavController
+    public navCtrl: NavController, private provider: Post
 ) {
 }
 
